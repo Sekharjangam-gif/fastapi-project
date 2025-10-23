a0ff8abc95d112233d8d5b5ad0c0c42800484731
Sekharjangam <jangamsekhar29@gmail.com>
2025-10-23 20:50:25 +0530

    added authorizations

--- app/core/security.py
+++ app/core/security.py
@@ -2,6 +2,7 @@ from datetime import datetime,timezone,timedelta
 from jose import jwt, JWTError
 from app.core.config import settings
 
+#creating the token here
 def create_token(data:dict, expire_minutes=30):
     to_encode= data.copy()
     expire=datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
@@ -11,7 +12,7 @@ def create_token(data:dict, expire_minutes=30):
 
     )
 
-
+#Varifying the toekn
 def verify_token(token:str):
     try:
         payload=jwt.decode(
